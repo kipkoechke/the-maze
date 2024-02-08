83182724c58298c5efb6fceac0e48e2c11b603f6
kipkoechke <chepkwonyke1@gmail.com>
2024-02-08 18:03:31 +0300

    Worked on processing inputs from the keyboard

--- src/input.c
+++ src/input.c
@@ -2,10 +2,11 @@
 /**
  * SDL_KEYDOWN_FUNC - process input when a key is down
  * @event: union that contains structures for the different event types
-*/
+ */
 
 void SDL_KEYDOWN_FUNC(SDL_Event event)
-{if (event.key.keysym.sym == SDLK_ESCAPE)
+{
+	if (event.key.keysym.sym == SDLK_ESCAPE)
 		GameRunning = false;
 	if (event.key.keysym.sym == SDLK_UP)
 		player.walkDirection = +1;
@@ -28,7 +29,7 @@ void SDL_KEYDOWN_FUNC(SDL_Event event)
 /**
  * SDL_KEYUP_FUNC - process input when a key is up
  * @event: union that contains structures for the different event types
-*/
+ */
 
 void SDL_KEYUP_FUNC(SDL_Event event)
 {
@@ -48,4 +49,22 @@ void SDL_KEYUP_FUNC(SDL_Event event)
 		player.turnDirection = 0;
 	if (event.key.keysym.sym == SDLK_d)
 		player.turnDirection = 0;
+}
+
+/**
+ * handleInput - process input from the keyboard
+ *
+ */
+void handleInput(void)
+{
+	SDL_Event event;
+
+	SDL_PollEvent(&event);
+
+	if (event.type == SDL_QUIT)
+		GameRunning = false;
+	else if (event.type == SDL_KEYDOWN)
+		SDL_KEYDOWN_FUNC(event);
+	else if (event.type == SDL_KEYUP)
+		SDL_KEYUP_FUNC(event);
 }
