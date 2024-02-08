7c4ba4213b1f04b81074d109214d7423bd917c92
kipkoechke <chepkwonyke1@gmail.com>
2024-02-08 18:48:10 +0300

    Rendered the floor projection

--- src/wall.c
+++ src/wall.c
@@ -16,3 +16,35 @@ void changeColorIntensity(color_t *color, float factor)
 
   *color = a | (r & 0x00FF0000) | (g & 0x0000FF00) | (b & 0x000000FF);
 }
+
+/**
+ * renderFloor - function to render the floor projection
+ *
+ * @WallBottomPixel: wall bottom px
+ * @texelColor: texture color for current px
+ * @x: current element in rays array
+ */
+
+void renderFloor(int wallBottomPixel, color_t *texelColor, int x)
+{
+  int y, texture_height, texture_width, textureOffsetY, textureOffsetX;
+  float distance, ratio;
+
+  texture_width = wallTextures[3].width;
+  texture_height = wallTextures[3].height;
+
+  for (y = wallBottomPixel - 1; y < SCREEN_HEIGHT; y++)
+  {
+    ratio = player.height / (y - SCREEN_HEIGHT / 2);
+    distance = (ratio * PROJ_PLANE) / cos(rays[x].rayAngle - player.rotationAngle);
+
+    textureOffsetY = (int)abs((distance * sin(rays[x].rayAngle)) + player.y);
+    textureOffsetX = (int)abs((distance * cos(rays[x].rayAngle)) + player.x);
+
+    textureOffsetX = (int)(abs(textureOffsetX * texture_width / 30) % texture_width);
+    textureOffsetY = (int)(abs(textureOffsetY * texture_height / 30) % texture_height);
+
+    *texelColor = wallTextures[4].texture_buffer[(texture_width * textureOffsetY) + textureOffsetX];
+    drawPixel(x, y, *texelColor);
+  }
+}
