3ccc6d661cb8546e98711854c91925d53f1f53b9
kipkoechke <chepkwonyke1@gmail.com>
2024-02-08 18:02:22 +0300

    Processed input when a key is up

--- src/input.c
+++ src/input.c
@@ -24,3 +24,28 @@ void SDL_KEYDOWN_FUNC(SDL_Event event)
 	if (event.key.keysym.sym == SDLK_d)
 		player.turnDirection = +1;
 }
+
+/**
+ * SDL_KEYUP_FUNC - process input when a key is up
+ * @event: union that contains structures for the different event types
+*/
+
+void SDL_KEYUP_FUNC(SDL_Event event)
+{
+	if (event.key.keysym.sym == SDLK_UP)
+		player.walkDirection = 0;
+	if (event.key.keysym.sym == SDLK_DOWN)
+		player.walkDirection = 0;
+	if (event.key.keysym.sym == SDLK_RIGHT)
+		player.turnDirection = 0;
+	if (event.key.keysym.sym == SDLK_LEFT)
+		player.turnDirection = 0;
+	if (event.key.keysym.sym == SDLK_w)
+		player.walkDirection = 0;
+	if (event.key.keysym.sym == SDLK_s)
+		player.walkDirection = 0;
+	if (event.key.keysym.sym == SDLK_a)
+		player.turnDirection = 0;
+	if (event.key.keysym.sym == SDLK_d)
+		player.turnDirection = 0;
+}
