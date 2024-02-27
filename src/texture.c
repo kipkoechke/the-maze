8baf55f0608c1dee24a8508fec335ab15e77cd35
kipkoechke <chepkwonyke1@gmail.com>
2024-02-27 17:08:16 +0300

    Implemented texture

---
+++ src/texture.c
@@ -0,0 +1,50 @@
+#include "../headers/header.h"
+
+static const char *textureFileNames[NUM_TEXTURES] = {
+    "./images/redbrick.png",
+    "./images/purplestone.png",
+    "./images/mossystone.png",
+    "./images/graystone.png",
+    "./images/colorstone.png",
+    "./images/bluestone.png",
+    "./images/wood.png",
+    "./images/eagle.png",
+};
+
+/**
+ * Loads textures into their respective positions.
+ */
+void WallTexturesready(void)
+{
+    int i;
+
+    for (i = 0; i < NUM_TEXTURES; i++)
+    {
+        upng_t *upng;
+
+        upng = upng_new_from_file(textureFileNames[i]);
+
+        if (upng != NULL)
+        {
+            upng_decode(upng);
+            if (upng_get_error(upng) == UPNG_EOK)
+            {
+                wallTextures[i].upngTexture = upng;
+                wallTextures[i].width = upng_get_width(upng);
+                wallTextures[i].height = upng_get_height(upng);
+                wallTextures[i].texture_buffer = (color_t *)upng_get_buffer(upng);
+            }
+        }
+    }
+}
+
+/**
+ * Frees wall textures.
+ */
+void freeWallTextures(void)
+{
+    int i;
+
+    for (i = 0; i < NUM_TEXTURES; i++)
+        upng_free(wallTextures[i].upngTexture);
+}
